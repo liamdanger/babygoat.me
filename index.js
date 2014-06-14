d8808b5961dcfee8db178b208f409ef562dcd44b
Liam Campbell <liam.campbell@moovweb.com>
2014-06-13 20:46:53 -0700

    Let's see if a simple app works

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-var express = require("express"),
-    jade = require("jade"),
-    request = require("request"),
-    app = express(),
+var app = express(),
     server;
 
 app.get("/", function(req, res) {
   console.log("sending a goat");
-  request("http://liamdanger.github.io/babygoat-motherlode/manifest.json", function(error, response, body) {
-    var data = JSON.parse(body);
-    var babygoats = data.babygoats;
-    var howManyGoats = babygoats.length;
-    var whichGoat = Math.floor(Math.random() * (howManyGoats));
-    res.render("index.jade", {babygoat: babygoats[whichGoat]});
-  });
+  // request("http://liamdanger.github.io/babygoat-motherlode/manifest.json", function(error, response, body) {
+  //   var data = JSON.parse(body);
+  //   var babygoats = data.babygoats;
+  //   var howManyGoats = babygoats.length;
+  //   var whichGoat = Math.floor(Math.random() * (howManyGoats));
+  //   res.render("index.jade", {babygoat: babygoats[whichGoat]});
+  // });
 });
 
 server = app.listen(3000, function() {
