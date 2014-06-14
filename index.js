2297386c60cf9bf0f915708a7fe0eb8ed1c9a9fd
Liam Campbell <liam.campbell@moovweb.com>
2014-06-13 22:39:55 -0700

    Fix errant parenthesis

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get("/", function(req, res) {
   });
 });
 
-server = app.listen(process.env.PORT || 3000), function() {
+server = app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
